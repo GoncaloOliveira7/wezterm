0519dc919a24fa5a03d152e7609decddde899131
Gonçalo Oliveira <goncalo@TUIL04HL6L913.local>
2024-11-28 18:22:50 +0000

    change theme based on system theme

--- wezterm.lua
+++ wezterm.lua
@@ -5,70 +5,76 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- For example, changing the color scheme:
-config.color_scheme = "One Light (base16)"
+local appearance_themes = {
+	Light = "One Light (base16)",
+	Dark = "dark_theme",
+}
+local appearance = wezterm.gui.get_appearance()
+config.color_scheme = appearance_themes[appearance]
 
 config.font_size = 16
 config.font = wezterm.font({
-        family = "MonacoB",
-        weight = "Bold",
+	family = "MonacoB",
+	weight = "Bold",
 })
 
 config.send_composed_key_when_left_alt_is_pressed = true
 config.send_composed_key_when_right_alt_is_pressed = true
+config.window_close_confirmation = "NeverPrompt"
 
 act = wezterm.action
 
 config.keys = {
-        {
-                key = "raw:36",
-                mods = "CMD|SHIFT",
-                action = act.TogglePaneZoomState,
-        },
-        {
-                key = "d",
-                mods = "CMD",
-                action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-        },
-        {
-                key = "d",
-                mods = "CMD|SHIFT",
-                action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
-        },
-        {
-                key = "w",
-                mods = "CMD",
-                action = act.CloseCurrentPane({ confirm = true }),
-        },
-        {
-                key = "LeftArrow",
-                mods = "CMD|SHIFT",
-                action = act.MoveTabRelative(-1),
-        },
-        {
-                key = "RightArrow",
-                mods = "CMD|SHIFT",
-                action = act.MoveTabRelative(1),
-        },
-        {
-                key = "LeftArrow",
-                mods = "CMD|OPT",
-                action = act.ActivatePaneDirection("Left"),
-        },
-        {
-                key = "RightArrow",
-                mods = "CMD|OPT",
-                action = act.ActivatePaneDirection("Right"),
-        },
-        {
-                key = "UpArrow",
-                mods = "CMD|OPT",
-                action = act.ActivatePaneDirection("Up"),
-        },
-        {
-                key = "DownArrow",
-                mods = "CMD|OPT",
-                action = act.ActivatePaneDirection("Down"),
-        },
+	{
+		key = "raw:36",
+		mods = "CMD|SHIFT",
+		action = act.TogglePaneZoomState,
+	},
+	{
+		key = "d",
+		mods = "CMD",
+		action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "d",
+		mods = "CMD|SHIFT",
+		action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "w",
+		mods = "CMD",
+		action = act.CloseCurrentPane({ confirm = true }),
+	},
+	{
+		key = "LeftArrow",
+		mods = "CMD|SHIFT",
+		action = act.MoveTabRelative(-1),
+	},
+	{
+		key = "RightArrow",
+		mods = "CMD|SHIFT",
+		action = act.MoveTabRelative(1),
+	},
+	{
+		key = "LeftArrow",
+		mods = "CMD|OPT",
+		action = act.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "RightArrow",
+		mods = "CMD|OPT",
+		action = act.ActivatePaneDirection("Right"),
+	},
+	{
+		key = "UpArrow",
+		mods = "CMD|OPT",
+		action = act.ActivatePaneDirection("Up"),
+	},
+	{
+		key = "DownArrow",
+		mods = "CMD|OPT",
+		action = act.ActivatePaneDirection("Down"),
+	},
 }
 
 -- and finally, return the configuration to wezterm
